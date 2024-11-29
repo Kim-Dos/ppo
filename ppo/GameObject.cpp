0afa639449efd0868f741d0799b590ded334b956
Dos-kim <glxmtmzlf@gmail.com>
2024-11-29 10:37:14 +0900

    too Merge

--- ppo/GameObject.cpp
+++ ppo/GameObject.cpp
@@ -214,82 +214,6 @@ void GameObject::SetTextureScale(XMFLOAT3 scale)
     mTexTransform = Matrix4x4::Multiply(mtxScale, mTexTransform);
 }
 
-<<<<<<< Updated upstream
-XMFLOAT4X4 GameObject::GetWorld()
-{
-    /*if (mWorldMatDirty)
-        UpdateWorldMatrix();*/
-
-    return mWorld;
-}
-
-XMFLOAT3 GameObject::GetPosition()
-{
-    return XMFLOAT3(mWorld._41, mWorld._42, mWorld._43);
-}
-
-XMFLOAT3 GameObject::GetLook()
-{
-    return Vector3::Normalize(XMFLOAT3(mWorld._31, mWorld._32, mWorld._33));
-}
-
-XMFLOAT3 GameObject::GetUp()
-{
-    return Vector3::Normalize(XMFLOAT3(mWorld._21, mWorld._22, mWorld._23));
-}
-
-XMFLOAT3 GameObject::GetRight()
-{
-    return Vector3::Normalize(XMFLOAT3(mWorld._11, mWorld._12, mWorld._13));
-}
-
-void GameObject::MoveStrafe(float distance)
-{
-    SetPosition(Vector3::Add(GetPosition(), GetRight(), distance));
-}
-
-void GameObject::MoveFront(float distance)
-{
-    XMFLOAT3 frontVector = GetLook();
-    frontVector.y = 0.0f;
-    SetPosition(Vector3::Add(GetPosition(), Vector3::Normalize(frontVector), distance));
-}
-
-void GameObject::MoveUp(float distance)
-{
-    SetPosition(Vector3::Add(GetPosition(), GetUp(), distance));
-}
-
-void GameObject::MoveForward(float distance)
-{
-    SetPosition(Vector3::Add(GetPosition(), GetLook(), distance));
-}
-
-void GameObject::Rotate(float pitch, float yaw, float roll)
-{
-    XMVECTOR rotation = XMQuaternionRotationMatrix(XMLoadFloat4x4(&mWorld));
-
-    XMMATRIX pitchRotation = XMMatrixRotationAxis(XMLoadFloat3(&GetRight()), pitch);
-    XMMATRIX yawRotation = XMMatrixRotationAxis(XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f), yaw);
-    XMMATRIX rollRotation = XMMatrixRotationAxis(XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f), roll);
-
-    XMMATRIX rotationMatrix = rollRotation * pitchRotation * yawRotation;
-
-    XMStoreFloat4x4(&mWorld, rotationMatrix * XMLoadFloat4x4(&mWorld));
-}
-
-void GameObject::Rotate(XMFLOAT3* axis, float angle)
-{
-    XMMATRIX rotateMat = XMMatrixRotationAxis(XMLoadFloat3(axis), XMConvertToRadians(angle));
-    mWorld = Matrix4x4::Multiply(rotateMat, mWorld);
-}
-
-void GameObject::Rotate(XMFLOAT4* quaternion)
-{
-    XMMATRIX rotateMat = XMMatrixRotationQuaternion(XMLoadFloat4(quaternion));
-    mWorld = Matrix4x4::Multiply(rotateMat, mWorld);
-}
-=======
 
 void GameObject::CreateBoundingBox(ID3D12Device* d3dDevice, ID3D12GraphicsCommandList* commandList)
 {
@@ -342,4 +266,3 @@ D3D12_INDEX_BUFFER_VIEW GameObject::BoundingBoxIndexBufferView() const
     }
 }
 
->>>>>>> Stashed changes
--- ppo/GameObject.h
+++ ppo/GameObject.h
@@ -5,45 +5,6 @@
 
 #define MAX_NUM_SUBMESHES 4
 
-//// 하나의 물체를 그리는 데 필요한 매개변수들을 담는 가벼운 구조체
-//struct RenderItem
-//{
-//	RenderItem() = default;
-//
-//	// 세계 공간을 기준으로 물체의 국소 공간을 서술하는 세계 행렬
-//	// 이 행렬은 세계 공간 안에서의 물체의 의치와 방행, 크기를 결정
-//	XMFLOAT4X4 World = MathHelper::Identity4x4();
-//
-//	XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
-//
-//	// 물체의 자료가 변해서 상수버퍼를 갱신해야 하는지의 여부를 뜻하는 'Dirty'플래그
-//	// FrameResource마다 물체의 cBuffer가 있으므로 물체의 자료를 수정할 떄는 반드시
-//	// NumFramesDirty = gNumFrameResources로 설정해야 한다.
-//	// 그래야 각각의 프레임 자원이 갱신된다.
-//	int NumFramesDirty = gNumFrameResources;
-//
-//	// 이 렌더 항목의 물체 상수 버퍼에 해당하는 
-//	// GPU 상수 버퍼의 색인
-//	UINT ObjCBIndex = -1;
-//
-//	// 이 렌더 항목에 연관된 기하구조. 여러 렌더항목이 같은 기하구조를 참조할 수 있다.
-//	Material* Mat = nullptr;
-//	Mesh* Geo = nullptr;
-//
-//	// Primitive topology.
-//	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
-//
-//	// DrawIndexedInstanced 매개변수들.
-//	UINT IndexCount = 0;
-//	UINT StartIndexLocation = 0;
-//	int BaseVertexLocation = 0;
-//
-//	// Only applicable to skinned render-items.
-//	UINT SkinnedCBIndex = -1;
-//
-//	// nullptr if this render-item is not animated by skinned mesh.
-//	//SkinnedModelInstance* SkinnedModelInst = nullptr;
-//};
 
 struct DrawIndex
 {
@@ -148,22 +109,11 @@ public:
 	std::string GetName() { return mName; }
 	
 	XMFLOAT4X4 GetTexTransform() { return mTexTransform; }
-<<<<<<< Updated upstream
 	XMFLOAT3 GetPosition();
 	XMFLOAT3 GetLook();
 	XMFLOAT3 GetUp();
 	XMFLOAT3 GetRight();
 
-	void MoveStrafe(float distance = 1.0f);
-	void MoveUp(float distance = 1.0f);
-	void MoveForward(float distance = 1.0f);
-	void MoveFront(float distance = 1.0f);
-
-	void Rotate(float pitch, float yaw, float roll);
-	void Rotate(XMFLOAT3* axis, float angle);
-	void Rotate(XMFLOAT4* quaternion);
-=======
-	
 	void CreateBoundingBox(ID3D12Device* d3dDevice, ID3D12GraphicsCommandList* commandList);
 
 	void SetBoundingBox(XMFLOAT3 center, XMFLOAT3 extents) { mBoundingBox = BoundingBox(center, extents); }
@@ -172,16 +122,12 @@ public:
 
 	D3D12_VERTEX_BUFFER_VIEW BoundingBoxVertexBufferView() const;
 	D3D12_INDEX_BUFFER_VIEW BoundingBoxIndexBufferView() const;
->>>>>>> Stashed changes
 private:
 	
-<<<<<<< Updated upstream
 	bool mWorldMatDirty = true;
 	XMFLOAT4X4 mWorld = MathHelper::Identity4x4();
-=======
 	BoundingBox mBoundingBox;
 
->>>>>>> Stashed changes
 	XMFLOAT4X4 mTexTransform = MathHelper::Identity4x4();
 
 	// 이 렌더 항목의 물체 상수 버퍼에 해당하는 
